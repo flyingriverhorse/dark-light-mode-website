0de5db359b95f16eb76b23e55871ab4480cbc809
flyingriverhorse <buzzycarl@gmail.com>
2025-03-09 16:23:20 +0200

    new changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const themeSwitch = document.getElementById('themeSwitch');
     const toggleIcon = document.getElementById('toggle-icon');
+    const navbar = document.getElementById('navbar');
   
     // Check localStorage for saved theme or default to light mode
     let theme = localStorage.getItem('theme') || 'light';
@@ -16,6 +17,9 @@ document.addEventListener('DOMContentLoaded', function () {
       if (theme === 'dark') {
         document.body.classList.remove('light-mode');
         document.body.classList.add('dark-mode');
+        // Update navbar classes for dark mode
+        navbar.classList.remove('navbar-light', 'bg-light');
+        navbar.classList.add('navbar-dark', 'bg-dark');
         // Update toggle text and icon
         toggleIcon.querySelector('.toggle-text').textContent = 'Dark Mode';
         toggleIcon.querySelector('i').classList.remove('fa-sun');
@@ -24,6 +28,9 @@ document.addEventListener('DOMContentLoaded', function () {
       } else {
         document.body.classList.remove('dark-mode');
         document.body.classList.add('light-mode');
+        // Update navbar classes for light mode
+        navbar.classList.remove('navbar-dark', 'bg-dark');
+        navbar.classList.add('navbar-light', 'bg-light');
         toggleIcon.querySelector('.toggle-text').textContent = 'Light Mode';
         toggleIcon.querySelector('i').classList.remove('fa-moon');
         toggleIcon.querySelector('i').classList.add('fa-sun');
